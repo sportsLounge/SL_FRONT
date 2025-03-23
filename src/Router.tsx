3b0d41b024bdc39f8b441a19c00a0a0971264218
JUMTBOX <yjey12@naver.com>
2025-03-23 12:14:34 +0900

    ...

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,9 @@ import Main from "./components/Main";
 export default function Router() {
   return (
     <Routes>
-<<<<<<< HEAD
-      <Route path="/sports_lounge" Component={() => <Main />} />
-=======
       <Route path="/" Component={() => <Main />} />
       <Route path="/somepath1" Component={() => <p>Temp route1</p>} />
       <Route path="/somepath2" Component={() => <p>Temp route2</p>} />
->>>>>>> 8d7f15a116af7ecab3634657f37a88657243462e
     </Routes>
   );
 }
