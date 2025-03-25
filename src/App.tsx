caf577056d19dec18b2807f304d19d4940c18913
JUMTBOX <yjey12@naver.com>
2025-03-25 21:19:43 +0900

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/customs/Header";
+import Header from "@/components/Header";
 import Router from "./Router";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="w-full h-full box-border flex-col justify-center items-center p-2">
+      <div className="">
         <Router />
       </div>
     </>
